f64303b0758617f26ea8648eeece408a6e6f1b64
TheFanMisPill <114370121+shinybka@users.noreply.github.com>
2023-10-28 20:36:06 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,20 +1,36 @@
-document.addEventListener('keydown', function (event) {
-    if (event.key === 'Enter') {
-        login();
-    }
-});
+// Sample local database (for demonstration purposes)
+const users = [
+  { username: 'admin', password: 'buynaa0401' },
+  { username: 'irmuun', password: 'oros320228b' },
+  { username: 'temka', password: 'oros320228b' },
+  { username: 'monhtulga', password: 'oros320228b' },
+  { username: 'tselmeg', password: 'oros320228b' },
+  { username: 'buji', password: 'oros320228b' },
+  // Add more users as needed
+];
 
-function login() {
-    var username = document.getElementById('username').value;
-    var password = document.getElementById('password').value;
+function checkLogin() {
+  const enteredUsername = document.getElementById('username').value;
+  const enteredPassword = document.getElementById('password').value;
 
-    // In a real-world scenario, you'd send the username and password to the server for authentication.
-    // For simplicity, let's hardcode a username and password for this example.
-    if (username === '8b' && password === '8b') {
-        alert('Login successful!');
-        window.location.href = 'Home/index.html';
-    } else {
-        alert('Invalid username or password.');
-        alert(' GO U TRAITOR ')
-    }
+  // Check if the entered username and password match any user in the database
+  const isValidLogin = users.some(user => user.username === enteredUsername && user.password === enteredPassword);
+
+  if (isValidLogin) {
+    alert('Login successful! Redirecting to another page...');
+    // Redirect to another HTML file or perform other actions as needed
+    window.location.href = 'Home/index.html';
+  } else {
+    alert('Invalid username or password. Please try again.');
+  }
 }
+
+// Listen for the "Enter" key press on the password input field
+document.getElementById('password').addEventListener('keydown', function(event) {
+  if (event.key === 'Enter') {
+    // Prevent the default form submission behavior
+    event.preventDefault();
+    // Call the checkLogin function when "Enter" is pressed
+    checkLogin();
+  }
+});
